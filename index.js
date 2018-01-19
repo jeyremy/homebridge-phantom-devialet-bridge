7dcf63b7314cbb55248d4f48a3db277ee214868d
st3v3nFr <thegeekfr@gmail.com>
2018-01-19 13:51:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 var Service, Characteristic;
+var discoverInterval = 10000; // rajouté pour pour les Devialets
 var Denon = require('./lib/denon');
 var inherits = require('util').inherits;
 var pollingtoevent = require('polling-to-event');
+var ssdp = require('node-ssdp').Client, client = new ssdp(), http = require('http'), url = require('url'), request = require('request'), actVol=0, actualDevice = []; // rajouté pour pour les Devialets
 
 
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
 
-    homebridge.registerAccessory('homebridge-denon-marantz-avr', 'DenonMarantzAVR', DenonAVRAccessory);
-};
+    homebridge.registerAccessory('DevialetPhantomDialog', DenonAVRAccessory);
+					};
 
 function DenonAVRAccessory(log, config) {
     this.log = log;
 	var that = this;
 	
     this.config = config;
-    this.ip = config['ip'];
+    this.devialet_ipaddress = config['ip']; // recuperer l'Ip dans Config pour le module devialet
     this.name = config['name'];
 
     this.defaultInput = config['defaultInput'] || null;
@@ -28,14 +30,14 @@ function DenonAVRAccessory(log, config) {
 	this.pollingInterval = config['pollingInterval'] || "60";
 	this.pollingInterval = parseInt(this.pollingInterval)
 
-    this.denon = new Denon(this.ip);
+    this.denon = new Denon(this.devialet_ipaddress);
 	
 	this.setAttempt = 0;
 	this.state = false;
 	if (this.interval < 10 && this.interval > 100000) {
 		this.log("polling interval out of range.. disabled polling");
 		this.doPolling = false;
-	}
+							  }
 
 	// Status Polling
 	if (this.doPolling) {
@@ -58,64 +60,6 @@ function DenonAVRAccessory(log, config) {
 	}
 }
 
-
-DenonAVRAccessory.prototype.getPowerState = function (callback, context) {
-	
-	if ((!context || context != "statuspoll") && this.doPolling) {
-		callback(null, this.state);
-	} else {
-	    this.denon.getPowerState(function (err, state) {
-	        if (err) {
-	            this.log(err);
-	            callback(null, false);
-	        } else {
-				this.log('current power state is: %s', (state) ? 'ON' : 'OFF');
-				callback(null, state);
-	        }
-	    }.bind(this));
-	}
-};
-
-
-DenonAVRAccessory.prototype.setPowerState = function (powerState, callback, context) {
-	var that = this;
-
-	//if context is statuspoll, then we need to ensure that we do not set the actual value
-	if (context && context == "statuspoll") {
-		callback(null, powerState);
-	    return;
-	}
-	
-	this.setAttempt = this.setAttempt+1;
-	
-    this.denon.setPowerState(powerState, function (err, state) {
-        if (err) {
-            this.log(err);
-        } else {
-            if(powerState && this.defaultInput) {
-                this.denon.setInput(this.defaultInput, function (error) {
-                    if (error) {
-                        this.log('Error setting default input. Please check your config');
-                    }
-                }.bind(this));
-            }
-            this.log('denon avr powered %s', state);
-        }
-    }.bind(this));
-
-    if (powerState && this.defaultVolume) {
-        setTimeout(function () {
-            this.denon.setVolume(this.defaultVolume, function (err) {
-                if (err) {
-                    this.log('Error setting default volume');
-                }
-                this.switchService.getCharacteristic(Characteristic.Volume)
-                  .updateValue(Math.round(this.defaultVolume / this.maxVolume * 100));
-            }.bind(this));
-        }.bind(this), 4000);
-    }
-    callback(null);
-};
 
 DenonAVRAccessory.prototype.getVolume = function (callback) {
     this.denon.getVolume(function (err, volume) {
@@ -142,44 +86,19 @@ DenonAVRAccessory.prototype.setVolume = function (pVol, callback) {
     }.bind(this))
 };
 
-DenonAVRAccessory.prototype.setMuteState = function (state, callback) {
-    this.denon.setMuteState(state, function (err) {
-        if (err) {
-            this.log('set mute error: ' + err);
-            callback(err);
-        } else {
-            callback(null);
-        }
-    }.bind(this));
-};
-
-DenonAVRAccessory.prototype.getMuteState = function (callback) {
-    this.denon.getMuteState(function (err, state) {
-        if (err) {
-            this.log('get mute error: ' + err);
-            callback(err);
-        } else {
-            callback(state);
-        }
-    }.bind(this))
-};
 
 DenonAVRAccessory.prototype.getServices = function () {
     var informationService = new Service.AccessoryInformation();
 
     informationService
         .setCharacteristic(Characteristic.Name, this.name)
-        .setCharacteristic(Characteristic.Manufacturer, this.type || 'Denon');
+        .setCharacteristic(Characteristic.Manufacturer,  'Devialet');
 
     this.switchService = new Service.Switch(this.name);
     this.switchService.getCharacteristic(Characteristic.On)
         .on('get', this.getPowerState.bind(this))
         .on('set', this.setPowerState.bind(this));
-
-    this.switchService.addCharacteristic(Characteristic.Mute)
-        .on('get', this.getMuteState.bind(this))
-        .on('set', this.setMuteState.bind(this));
-
+// J'ai enlevé la partie Mute ici
     this.switchService.addCharacteristic(Characteristic.Volume)
         .on('get', this.getVolume.bind(this))
         .on('set', this.setVolume.bind(this));
